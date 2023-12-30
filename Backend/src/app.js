7ee6bc84a70ffb22d6eea94cae7a27d5762272cd
B. Deewakar Rao <b.deewakarrao@Bs-MacBook-Air.local>
2023-12-31 02:12:17 +0530

    added database

---
+++ Backend/src/app.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit:"16kb"}))
+app.use(express.static("public"))
+
+export {app, port}
---
+++ Backend/src/index.js
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv'
+import connectDB from './db/index.js';
+import { app,port } from './app.js';
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+.then(()=>{
+    app.listen(port , ()=>{
+        console.log(`Server is running at: http://localhost:${port}`)
+    })
+})
+.catch((error)=>{
+    console.log(error);
+})
