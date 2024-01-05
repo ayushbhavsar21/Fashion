c581bda2e65d3f3a6400929f368b230491d70e43
B. Deewakar Rao <deewakarrao59@gmail.com>
2024-01-05 18:26:02 +0530

    added product and category models

---
+++ Backend/src/models/product.models.js
@@ -0,0 +1,46 @@
+import { Schema,model } from 'mongoose';
+
+const productSchema = new Schema(
+  {
+    description: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    productImage: {
+      type: String,
+    },
+    price: {
+      type: Number,
+      default: 0,
+    },
+    stock: {
+      type: Number,
+      default: 0,
+    },
+    category: {
+      type: Schema.Types.ObjectId,
+      ref: 'Category',
+      required: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ['PENDING', 'CANCELLED', 'DELIVERED'],
+      default: 'PENDING',
+    },
+  },
+  { timestamps: true }
+);
+
+export const Product = model('Product', productSchema);
