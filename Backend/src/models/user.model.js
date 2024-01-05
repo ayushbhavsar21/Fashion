20f21b1c9f01960f5d0d8968e3667aa33f247a7d
B. Deewakar Rao <b.deewakarrao@Bs-MacBook-Air.local>
2024-01-05 13:10:34 +0530

    created user model

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -1,7 +1,22 @@
-import { Schema, model } from "mongoose";
+import { Schema, model } from "mongoose"
 
-const userSchema = new Schema({
-
-}, {timestamps: true});
-
-export const User = model("User", userSchema);
+const userSchema = new mongoose.Schema({
+    userName: {
+      type: String,
+      unique: true,
+      required: [true, "Username is required!!"],
+      lowercase: true
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required!!"],
+      unique: true,
+      lowercase: true
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required!!"],
+    }
+  },{timestamps: true})
+  
+  export const User = mongoose.model("User", userSchema)
