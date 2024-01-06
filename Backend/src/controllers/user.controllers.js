442a997d5d6ff1385bc57dca24c2889763ec7e5b
B. Deewakar Rao <deewakarrao59@gmail.com>
2024-01-06 15:50:50 +0530

    added api error and response utils

--- Backend/src/controllers/user.controllers.js
+++ Backend/src/controllers/user.controllers.js
@@ -1,6 +1,12 @@
 import { asyncHandler } from "../utils/asyncHandler";
+import { User } from "../models/user.models";
 
 const registerUser = asyncHandler(async(req,res)=>{
+
+    const {email, userName, password} = req.body;
+
+    const existedUser = await User.findOne({email});
+
 
 });
 
