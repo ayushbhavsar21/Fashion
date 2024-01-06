2ea1c676b00f29b29d68458e82bc069da809e553
ayushbhavsar21 <ayush21bhavsar@gmail.com>
2024-01-06 13:11:40 +0530

    a

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
+import SignIn from './components/SignIn';
 import Home from './pages/Home';
+
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <Router>
       <Routes>
       <Route exact path="/" element={<Home />} />
+      <Route exact path="/SignIn" element={<SignIn />} />
       </Routes>
     </Router>
   )
---
+++ Frontend/src/components/SignIn.jsx
@@ -0,0 +1,10 @@
+import Navbar from "./Navbar";
+function SignIn(){
+    return(
+        <>
+            <h1 className=" h-[10px]">HELLO WORLD</h1>
+            
+        </>
+    );
+}
+export default SignIn
