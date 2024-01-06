9098cfb586a102d2b6df72dc274a34357419d0e4
B. Deewakar Rao <deewakarrao59@gmail.com>
2024-01-06 16:23:28 +0530

    added refresh acces tokens and bcrypt hash

--- Backend/src/models/user.models.js
+++ Backend/src/models/user.models.js
@@ -1,4 +1,6 @@
 import {Schema, model} from 'mongoose'
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const userSchema = new Schema({
   userName: {
@@ -18,5 +20,42 @@ const userSchema = new Schema({
     required: [true, "Password is required!!"],
   }
 },{timestamps: true})
+
+userSchema.pre("save", async function(next){
+  if(this.isModified("password")){
+    this.password = await bcrypt(this.password, 10);
+    next();
+  }
+  else{
+    next();
+  }
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+  return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = function(){
+  return jwt.sign({
+    _id: this._id,
+    email: this.email,
+    userName: this.userName,
+    password: this.password
+  },
+    process.env.ACCESS_TOKEN_SECRET,
+  {
+    expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+  })
+}
+
+userSchema.methods.generateRefreshToken = async function(){
+  return jwt.sign({
+    _id: this._id,
+  },
+    process.env.ACCESS_TOKEN_SECRET,
+  {
+    expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+  })
+}
 
 export const User = model("User", userSchema)
