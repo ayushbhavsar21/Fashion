0e1ca2cd637aa7a99bc62899a20a1fde6cc32923
AviralPatel0526 <aviralpatel2603@gmail.com>
2024-01-07 11:41:41 +0530

    added footer and summer section

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import Section1 from '../components/Section1'
 import Section2 from '../components/Section2'
 import Section3 from '../components/Section3'
+import Section4 from '../components/Section4'
+import { FooterFour } from '../components/FooterFour'
 import Navbar from '../components/Navbar'
 const Home = () => {
     return(
@@ -9,6 +11,8 @@ const Home = () => {
     <Section1/>
     <Section2/>
     <Section3/>
+    <Section4/>
+    <FooterFour/>
    </div>
     );
 };
