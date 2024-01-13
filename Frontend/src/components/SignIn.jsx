26c543f8bddc621e9a9063870bb4f2593161e8ae
AviralPatel0526 <aviralpatel2603@gmail.com>
2024-01-13 12:12:57 +0530

    made sign in page responsive and made it into a non scrollable page

--- Frontend/src/components/SignIn.jsx
+++ Frontend/src/components/SignIn.jsx
@@ -2,7 +2,7 @@ import Navbar from "./Navbar";
 import { useState } from "react";
 import SignInclipart from "../assets/SignIn-Clipart.svg"
 import line from "../assets/line.png"
-import Google from "../assets/Google.png"
+import Google from "../assets/googleLogo.svg"
 function SignIn() {
     
     const [user, setUser] = useState({
@@ -27,34 +27,44 @@ function SignIn() {
         < >
 
             <Navbar />
-            <div className="bg-primary h-[110vh] w-[100%] flex max-[750px]:hidden ">
-                <img className=" ml-[5vw] drop-shadow-2xl w-[50%] mb-[8vh] " src={SignInclipart} alt="" />
-                <div className="w-[50%]">
-                    <div className=" mt-[9vh] rounded-lg bg-tertiary w-[80%] h-[70vh] flex flex-col ">
-                        <div className=" mt-[5vh] ml-[3vw]">Email</div>
-                        <input className=" mt-[2vh] ml-[3vw] mr-[3vw] h-[5vh] rounded-lg " type="text" name="email" value={user.email} onChange={handleInput} />
-                        <div className=" mt-[2vh] ml-[3vw]">Password</div>
-                        <input className=" mt-[2vh] ml-[3vw] mr-[3vw] h-[5vh] rounded-lg " type="text" name="password" value={user.password} onChange={handleInput} />
-                        <button onClick={handleSubmit} className=" ml-[36%] mt-[2vh] w-[10vw]   border-2 border-secondary drop-shadow-[0_5px_5px_rgba(58,163,159,0.8)] bg-secondary  rounded-lg text-white flex justify-center " >
-                            <p className="  text-[16px] md:text-[18px] lg:text-[22px] ">SignIn</p>
-                        </button>
-                        <img className="mt-[4vh] drop-shadow-2xl" src={line} alt="" />
+            <div className="bg-primary h-[87vh] w-[100%] flex justify-around font-playfair">
+                <div className="w-[50vw] lg:flex justify-end items-center hidden">
+                <img className="drop-shadow-2xl w-[90%] relative left-16 bottom-16" src={SignInclipart} alt="" />
+                 
+                </div>
+                <div className="lg:w-[50vw] md:w-[60vw] sm:w-[75vw] w-[95vw] flex flex-col lg:justify-start pt-2 lg:items-start items-center justify-center">
+                    <div className="rounded-[35px] bg-tertiary sm:w-[70%] w-[100%] h-[62vh] flex flex-col gap-1 p-8">
+                        <div className="">
+                        <div className="pl-1 pb-2">Email</div>
+                        <input className="rounded-lg h-[45px] w-[100%]" type="text" name="email" value={user.email} onChange={handleInput} />
+                        </div>
+                        
+                        <div>
+                        <div className="pl-1 pb-2">Password</div>
+                        <input className="h-[45px] w-[100%]  rounded-lg " type="text" name="password" value={user.password} onChange={handleInput} />
+                        </div>
 
-                        <button className="flex rounded-lg border-2 border-black ml-[15%] mr-[15%] items-center bg-primary drop-shadow-2xl " >
-                            <img className=" ml-[4vw] h-[5vh] w-[3vw]" src={Google} alt="Google" />
-                            <p className="  ml-[2vw] text-[16px] md:text-[18px] lg:text-[22px] ">SignIn with Google</p>
+                        <button onClick={handleSubmit} className="drop-shadow-[0_5px_5px_rgba(58,163,159,0.8)] px-4 py-3 w-[120px] bg-secondary text-white  rounded-md self-center mt-4" >
+                           SignIn
+                        </button>
+
+                        <img className=" drop-shadow-2xl " src={line} alt="" />
+
+                        <button className="flex justify-center gap-4 rounded-lg border-[1px] border-black items-center bg-primary drop-shadow-2xl w-[80%] self-center p-2" >
+                            <img className="h-[32px]" src={Google} alt="Google" />
+                            <p className="">Sign In with Google</p>
                         </button>
                     </div>
-                    <div className=" mt-[4vh] w-[80%] h-[10vh] flex flex-col items-center">
-                        <div className="text-[24px]">Don't Have Account ?</div>
+                    <div className=" w-[80%] flex flex-col items-center lg:pr-12 mt-4 gap-3">
+                        <div className="">Don't Have Account ?</div>
                         <a href="/Buyerregister">
-                            <button className="mt-[2vh] flex   border-2 border-secondary drop-shadow-[0_5px_5px_rgba(58,163,159,0.8)] bg-secondary  rounded-lg text-white  " >
-                                <p className="  w-[24vw] text-[14px] max-[1100px]:text-[16px] xl:text-[22px]  ">Create an Buyer's Account</p>
+                            <button className="flex   border-2 border-secondary drop-shadow-[0_5px_5px_rgba(58,163,159,0.8)] bg-secondary  rounded-md text-white  px-3 py-2" >
+                                <p className="">Create an Buyer's Account</p>
                             </button>
                         </a>
                         <a href="/Sellerregister">
-                            <button className=" mt-[2vh] flex   border-2 border-secondary drop-shadow-[0_5px_5px_rgba(58,163,159,0.8)] bg-secondary  rounded-lg text-white " >
-                                <p className=" w-[24vw] text-[14px] max-[1100px]:text-[16px] xl:text-[22px] ">Create an Seller's Account</p>
+                            <button className=" flex   border-2 border-secondary drop-shadow-[0_5px_5px_rgba(58,163,159,0.8)] bg-secondary  rounded-md px-3 py-2 text-white " >
+                                <p className="  ">Create an Seller's Account</p>
                             </button>
                         </a>
                     </div>
