09cea0674d17ad94f225aa7b5e5e570c972f6809
AviralPatel0526 <aviralpatel2603@gmail.com>
2024-02-01 00:23:45 +0530

    .

--- Frontend/src/categories/Shoes.jsx
+++ Frontend/src/categories/Shoes.jsx
@@ -7,7 +7,7 @@ function Shoes() {
 
     
 <div className='mt-8'>
-      <h1 className='font-dancing sm:text-[36px] text-[20px] text-gray-900 pl-32'><span className='sm:text-[64px] text-[40px]'>Shoes </span> </h1> 
+      <h1 className='font-dancing sm:text-[36px] text-[20px] text-gray-900 sm:pl-32 pl-4'><span className='sm:text-[64px] text-[40px]'>Shoes </span> </h1> 
       </div>
         <div className="mx-auto grid w-full max-w-7xl items-center space-y-4 px-2 py-10 md:grid-cols-2 md:gap-6 md:space-y-0 lg:grid-cols-4">
           {Array.from({ length: 8 }).map((_, i) => (
