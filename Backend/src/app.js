73435bf6ad9896419f5b1c425e5c630110b3c5fa
B. Deewakar Rao <deewakarrao59@gmail.com>
2024-06-16 17:07:16 +0530

    changes in app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,9 +2,6 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
-import passport from 'passport';
-import { Strategy as LocalStrategy } from 'passport-local';
-import GoogleStrategy from 'passport-google-oauth2';
 
 // Initialize express app
 const app = express();
@@ -21,25 +18,6 @@ app.use(cookieParser());
 app.use(express.json({ limit: "16kb" }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
-// Passport Google OAuth setup
-passport.use('google', 
-    new GoogleStrategy({
-        clientID: process.env.GOOGLE_CLIENT_ID, 
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
-        callbackURL: "http://localhost:5173/auth/google/",
-        userProfileURL: "http://www.googleapis.com/oauth2/v3/userinfo",
-    }, async (accessToken, refreshToken, profile, cb) => {
-        console.log(profile);
-        // Handle profile information here
-        cb(null, profile);
-    })
-);
-
-// Google OAuth authentication route
-app.get("http://localhost:5173/auth/google", passport.authenticate("google", {
-    scope: ["profile", "email"],
-}));
 
 // Routes
 import userRouter from './routes/user.routes.js';
